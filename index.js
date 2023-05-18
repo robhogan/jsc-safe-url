20aa3024bbeb437bec4fee8f924d94497a64b16c
Rob Hogan <rob@rhogan.net>
2023-05-18 16:09:46 +0100

    Switch to flotate types within main source, add package.json#files

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
  * introduced in iOS 16.4.
  *
  * See https://github.com/facebook/react-native/issues/36794 for context.
+ *
+ * @flow strict
  */
 
 // We use regex-based URL parsing as defined in RFC3986 because it's easier to
@@ -14,8 +16,11 @@
 // https://www.rfc-editor.org/rfc/rfc3986#appendix-B
 const URI_REGEX = /^(([^:/?#]+):)?(\/\/([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/;
 
-function _rfc3986Parse(url) {
+function _rfc3986Parse(url /*:string */) {
   const match = url.match(URI_REGEX);
+  if (match == null) {
+    throw new Error("Unexpected error - failed to regex-match URL");
+  }
   return {
     schemeAndAuthority: (match[1] || "") + (match[3] || ""),
     path: match[5] || "",
@@ -25,7 +30,7 @@ function _rfc3986Parse(url) {
   };
 }
 
-function isJscSafeUrl(url) {
+function isJscSafeUrl(url /*:string */) /*:boolean*/ {
   const parsedUrl = _rfc3986Parse(url);
   return !parsedUrl.hasQueryPart;
 }
@@ -34,7 +39,7 @@ function isJscSafeUrl(url) {
  * @param {string} urlToNormalize
  * @returns string
  */
-function toNormalUrl(urlToNormalize) {
+function toNormalUrl(urlToNormalize /*:string */) /*:string */ {
   const parsedUrl = _rfc3986Parse(urlToNormalize);
   if (parsedUrl.path.indexOf("//&") === -1) {
     return urlToNormalize;
@@ -57,7 +62,7 @@ function toNormalUrl(urlToNormalize) {
  * @param {string} urlToConvert
  * @returns string
  */
-function toJscSafeUrl(urlToConvert) {
+function toJscSafeUrl(urlToConvert /*:string */) /*:string */ {
   if (!_rfc3986Parse(urlToConvert).hasQueryPart) {
     return urlToConvert;
   }
